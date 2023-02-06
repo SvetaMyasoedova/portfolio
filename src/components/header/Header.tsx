6a0a499aa9ce2900eb5746ea1861cd4eabacde01
SvetaMyasoedova <so.myasoedova@gmail.com>
2023-02-06 16:33:45 +0700

    linl to main page was added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,10 @@ const Header: FC = () => {
   return (
     <div>
       <header className={headerStyles.header}>
-        <p>SvetaMyasoedova</p>
+        <Link to="/">
+          <p className={headerStyles.name}>SvetaMyasoedova</p>
+        </Link>
+
         <nav className={headerStyles.nav}>
           <Link to="/about">
             <p className={headerStyles.content}>About</p>
