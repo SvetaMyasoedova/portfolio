7f06d6a347df3e130e813f1318358d6d996c6e53
SvetaMyasoedova <so.myasoedova@gmail.com>
2023-03-02 11:18:30 +0100

    data of library project were added

--- src/data/project.js
+++ src/data/project.js
@@ -1,4 +1,4 @@
-//projects
+//burger
 import feeds from "../images/feeds.png";
 import ingredientDetails from "../images/ingredient_details.png";
 import login from "../images/login.png";
@@ -6,6 +6,11 @@ import mainPage from "../images/main_page.png";
 import modal from "../images/modal.png";
 import order from "../images/order.png";
 import register from "../images/register.png";
+
+//library
+import add from "../images/add.png";
+import cards from "../images/cards.png";
+import library from "../images/library.png";
 
 export const projects = [
   {
@@ -32,6 +37,23 @@ export const projects = [
       <img src={feeds} alt="the feeds page" />,
       <img src={register} alt="register page" />,
       <img src={login} alt="login page" />,
+    ],
+  },
+  {
+    id: 2,
+    title: "LOrem ipsum",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+    skills: [
+      "Javascript",
+      "Local Storage",
+      "CSS"
+
+    ],
+    images: [
+      <img src={library} alt="the main library page" />,
+      <img src={add} alt="add modal" />,
+      <img src={cards} alt="cards with books" />,
+      
     ],
   },
 ];
