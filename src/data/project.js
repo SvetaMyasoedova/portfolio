cc778322c42d833a213b28ff1fa9205aee8df62c
SvetaMyasoedova <so.myasoedova@gmail.com>
2023-03-02 12:11:13 +0100

    data of corona project were added

--- src/data/project.js
+++ src/data/project.js
@@ -17,6 +17,12 @@ import ticTacToe from "../images/tic_tac_toe.png";
 import win from "../images/win.png";
 import tie from "../images/tie.png";
 
+//corona
+import corona from "../images/corona.png";
+import app from "../images/app.png";
+import facts from "../images/facts.png";
+import appDownload from "../images/app_download.png";
+
 export const projects = [
   {
     id: 1,
@@ -64,6 +70,18 @@ export const projects = [
       <img src={ticTacToe} alt="the main Tic Tac Toe page" />,
       <img src={win} alt="the win situation" />,
       <img src={tie} alt="It's a tie!" />,
+    ],
+  },
+  {
+    id: 4,
+    title: "LOrem ipsum",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+    skills: ["HTML", "SCSS"],
+    images: [
+      <img src={corona} alt="the main Corons page" />,
+      <img src={app} alt="about app" />,
+      <img src={facts} alt="facts about Corona" />,
+      <img src={appDownload} alt="app download" />,
     ],
   },
 ];
