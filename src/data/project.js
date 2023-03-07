bfd56c0b539392ddc7bcdb7cc57951fb1b606e4f
SvetaMyasoedova <so.myasoedova@gmail.com>
2023-03-07 16:36:14 +0100

    GoCorona's description was added

--- src/data/project.js
+++ src/data/project.js
@@ -77,8 +77,8 @@ export const projects = [
   },
   {
     id: 4,
-    title: "LOrem ipsum",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+    title: "GoCorona",
+    text: "Simple responsive website. I used this free design to learn how to layout and make responsive websites. Also SCSS is actively used here.",
     skills: ["HTML", "SCSS"],
     images: [
       <img src={corona} alt="the main Corons page" />,
