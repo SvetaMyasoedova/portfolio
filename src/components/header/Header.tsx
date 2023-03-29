6b714d746db96a0e198929451cebde84cf079e68
SvetaMyasoedova <so.myasoedova@gmail.com>
2023-03-29 13:44:22 +0200

    burger menu was added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,12 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Link } from "react-router-dom";
 import headerStyles from "./header.module.css";
 
 const Header: FC = () => {
+  const [isBurgerMenu, setIsBurgerMenu] = useState(false);
+  const handleLinkClick = () => {
+    setIsBurgerMenu(false);
+  };
   return (
     <div>
       <header className={headerStyles.header}>
@@ -10,18 +14,38 @@ const Header: FC = () => {
           <p className={headerStyles.name}>SvetaMyasoedova</p>
         </Link>
 
-        <nav className={headerStyles.nav}>
-          <Link to="/about">
+        <div
+          className={
+            headerStyles.burger +
+            " " +
+            (isBurgerMenu && headerStyles.burgerActive)
+          }
+          onClick={() => {
+            setIsBurgerMenu(!isBurgerMenu);
+          }}
+        >
+          <span className={headerStyles.line}></span>
+          <span className={headerStyles.line}></span>
+          <span className={headerStyles.line}></span>
+        </div>
+
+        <nav
+          className={
+            headerStyles.nav +
+            " " +
+            (isBurgerMenu && headerStyles.navActive)
+          }
+        >
+          <Link onClick={handleLinkClick} to="/about">
             <p className={headerStyles.content}>About</p>
           </Link>
 
-          <Link to="/projects">
+          <Link onClick={handleLinkClick} to="/projects">
             <p className={headerStyles.content}>Projects</p>
           </Link>
-          <Link to="/contact">
-          <p className={headerStyles.content}>Contact</p>
+          <Link onClick={handleLinkClick} to="/contact">
+            <p className={headerStyles.content}>Contact</p>
           </Link>
-         
         </nav>
       </header>
     </div>
